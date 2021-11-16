f709df05063c359fa251e87c71b40e0c5156ce53
Guoligu <751051329@qq.com>
2021-11-16 14:30:00 +0800

    bugfix

--- src/main/java/leet/soluiton/P4.java
+++ src/main/java/leet/soluiton/P4.java
@@ -7,8 +7,8 @@ package leet.soluiton;
 public class P4 {
     public static void main(String[] args) {
         P4 p = new P4();
-        int[] nums1 = {1};
-        int[] nums2 = {3};
+        int[] nums1 = {};
+        int[] nums2 = {2, 3, 4};
         double ans = p.findMedianSortedArrays(nums1, nums2);
         System.out.println(ans);
     }
@@ -41,16 +41,30 @@ public class P4 {
      * @param k 返回两个数组中第k大的元素，k即要剪裁的元素个数
      */
     public int getElem(int[] nums1, int[] nums2, int k) {
+        int n = nums1.length;
+        int m = nums2.length;
         // 数组中被剪裁的最后一个元素的位置
         int pos1 = -1;
         int pos2 = -1;
 
-        while (k > 0) {
+        while (true) {
+            // 整个数组被剪裁的情况
+            if (pos1 == n-1) {
+                return nums2[pos2+1+k];
+            }
+            if (pos2 == m-1) {
+                return nums1[pos1+1+k];
+            }
+            // 剪裁完毕
+            if (k == 0) {
+                return Math.min(nums1[pos1+1], nums2[pos2+1]);
+            }
+
             // mid表示此轮循环要剪裁的元素个数，采用二分的方式
             int mid = (k == 1 ? 1 : k / 2);
             // 一种值得学习的边界判断方式
-            int nextPos1 = Math.min(mid + pos1, nums1.length-1);
-            int nextPos2 = Math.min(mid + pos2, nums2.length-1);
+            int nextPos1 = Math.min(mid + pos1, n-1);
+            int nextPos2 = Math.min(mid + pos2, m-1);
 
             if ( nums1[nextPos1] < nums2[nextPos2] ) {
                 k -= (nextPos1 - pos1);
@@ -61,14 +75,6 @@ public class P4 {
             }
         }
 
-        // 整个数组被剪裁的情况
-        if (pos1 == nums1.length-1) {
-            return nums2[pos2+1];
-        }
-        if (pos2 == nums2.length-1) {
-            return nums1[pos1+1];
-        }
 
-        return Math.min(nums1[pos1+1], nums2[pos2+1]);
     }
 }
